b2f953f2db4edc4ed0c3452dc4605f519db03f27
koala819 <x.genolhac@gmail.com>
2021-04-04 11:09:47 +0200

    change quotes in index.js

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
-const buttonAdd = document.getElementById("addButton");
+const buttonAdd = document.getElementById('addButton');
 
-buttonAdd.addEventListener("click", () => {
+buttonAdd.addEventListener('click', () => {
     alert('Pas encore implémenté');
 })
