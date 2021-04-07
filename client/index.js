785b6a588ec431fc8125f275a502daf011f22369
koala819 <x.genolhac@gmail.com>
2021-04-07 12:50:41 +0200

    create Item class

--- client/index.js
+++ client/index.js
@@ -1,3 +1,10 @@
+class Item {
+    constructor(name, checked = false) {
+        this.name = name;
+        this.checked = checked;
+    }
+}
+
 const buttonAdd = document.getElementById('addButton');
 
 buttonAdd.addEventListener('click', () => {
