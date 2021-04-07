26a46921e6424aea01393fb6600505b6c496273e
koala819 <x.genolhac@gmail.com>
2021-04-07 13:16:09 +0200

    display the shopping list with JS

--- client/index.js
+++ client/index.js
@@ -9,6 +9,11 @@ const butter = new Item('beurre');
 const tomatoes = new Item('tomates');
 const garlic = new Item('ail');
 const shoppingList = [butter, tomatoes, garlic];
+let shoppingListContent = '';
+shoppingList.forEach(item => {
+    shoppingListContent += `<input type="checkbox"><label>${item.name}</label>`;
+});
+document.getElementById('shoppingList').innerHTML = shoppingListContent;
 
 const buttonAdd = document.getElementById('addButton');
 
