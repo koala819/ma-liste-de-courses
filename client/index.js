02e3927f7111ebc0b77fecb87084c1d69b106755
koala819 <x.genolhac@gmail.com>
2021-04-09 13:12:05 +0200

    add item to the list

--- client/index.js
+++ client/index.js
@@ -10,14 +10,20 @@ const butter = new Item('beurre salé', 'beurre');
 const tomatoes = new Item('tomates');
 const garlic = new Item('ail');
 const shoppingList = [butter, tomatoes, garlic];
+transformItemListToHtml(shoppingList);
 
-const shoppingListContent = shoppingList.map(item => {
-    return `<input type="checkbox" id="${item.id}"><label for="${item.id}">${item.name}</label>`;
-});
+function transformItemListToHtml(shoppingList) {
+    const shoppingListContent = shoppingList.map(item => {
+        return `<input type="checkbox" id="${item.id}"><label for="${item.id}">${item.name}</label>`;
+    });
+    document.getElementById('shoppingList').innerHTML = shoppingListContent.join('');
+}
 
-document.getElementById('shoppingList').innerHTML = shoppingListContent.join('');
 const buttonAdd = document.getElementById('addButton');
-
-buttonAdd.addEventListener('click', () => {
-    alert('Pas encore implémenté');
-})
+buttonAdd.addEventListener('click', (event) => {
+    event.preventDefault();
+    const addInputValue = document.getElementById('addInput').value;
+    const item = new Item(addInputValue);
+    shoppingList.push(item);
+    transformItemListToHtml(shoppingList);
+});
