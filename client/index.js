0ec488c2c02985b5a6fce41d1342b612c99cea89
koala819 <x.genolhac@gmail.com>
2021-04-20 12:55:17 +0200

    rename checked into class Product to bought

--- client/index.js
+++ client/index.js
@@ -1,8 +1,8 @@
 class Product {
-    constructor(name, id, checked = false) {
+    constructor(name, id, bought = false) {
         this.name = name;
         this.id = id ? id : name;
-        this.checked = checked;
+        this.bought = bought;
     }
 }
 
@@ -34,7 +34,7 @@ function createInputFromProduct(product) {
     const input = document.createElement('input');
     input.type = 'checkbox';
     input.id = product.id;
-    if (product.checked) {
+    if (product.bought) {
         input.checked = true;
     }
     const label = document.createElement('label');
