172cb477ec5e01f23128706ad178f7d2c1c0125a
koala819 <x.genolhac@gmail.com>
2021-04-27 11:24:04 +0200

    improve code to put funtions in class

--- client/index.js
+++ client/index.js
@@ -6,10 +6,10 @@ class ShoppingList {
     toHtml() {
         document.getElementById('shoppingList').innerHTML = '';
         this.products.forEach(product => {
-            const input = createInputFromProduct(product);
+            const input = product.createInputFrom(product);
             document.getElementById('shoppingList').appendChild(input);
             input.addEventListener('click', (event) => {
-                inputFromItemCheckedNew(product);
+                product.inputFromItemCheckedNew(product);
             })
         });
     }
@@ -23,6 +23,21 @@ class ShoppingList {
         };
         return false;
     }
+
+    addListenerToButton(event) {
+        event.preventDefault();
+        const addInputElement = document.getElementById('addInput')
+        const product = new Product(addInputElement.value);
+        if (addInputElement.value === '') {
+            alert('merci de saisir un produit à rajouter à la liste');
+        } else if (shoppingList.contains(product)) {
+            alert('---------------\nCe produit a déjà été saisi !\n---------------');
+        } else {
+            shoppingList.products.push(product);
+            shoppingList.toHtml();
+            addInputElement.value = '';
+        }
+    }
 }
 
 class Product {
@@ -31,53 +46,41 @@ class Product {
         this.id = id ? id : name;
         this.bought = bought;
     }
+
+    createInputFrom(product) {
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        input.id = product.id;
+        if (product.bought) {
+            input.checked = true;
+        }
+        const label = document.createElement('label');
+        label.for = product.id;
+        label.innerText = product.name;
+        const div = document.createElement('div');
+        div.appendChild(input);
+        div.appendChild(label);
+        return div;
+    }
+
+    inputFromItemCheckedNew(product) {
+        const checkbox = document.getElementById(product.id);
+        if (product.bought === false) {
+            checkbox.checked = true;
+            product.bought = true;
+        } else {
+            checkbox.checked = false;
+            product.bought = false;
+        }
+    }
 }
 
 const shoppingList = new ShoppingList([]);
 shoppingList.toHtml();
 
-//SHOPPING LIST
+const product = new Product();
+
 const buttonAdd = document.getElementById('addButton');
 buttonAdd.addEventListener('click', (event) => {
-    event.preventDefault();
-    const addInputElement = document.getElementById('addInput')
-    const product = new Product(addInputElement.value);
-    if (addInputElement.value === '') {
-        alert('merci de saisir un produit à rajouter à la liste');
-    } else if (shoppingList.contains(product)) {
-        alert('---------------\nCe produit a déjà été saisi !\n---------------');
-    } else {
-        shoppingList.products.push(product);
-        shoppingList.toHtml();
-        addInputElement.value = '';
-    }
+    shoppingList.addListenerToButton(event);
 });
-
-//PRODUCT
-function createInputFromProduct(product) {
-    const input = document.createElement('input');
-    input.type = 'checkbox';
-    input.id = product.id;
-    if (product.bought) {
-        input.checked = true;
-    }
-    const label = document.createElement('label');
-    label.for = product.id;
-    label.innerText = product.name;
-    const div = document.createElement('div');
-    div.appendChild(input);
-    div.appendChild(label);
-    return div;
-}
-
-//PRODUCT
-function inputFromItemCheckedNew(product) {
-    const checkbox = document.getElementById(product.id);
-    if (product.bought === false) {
-        checkbox.checked = true;
-        product.bought = true;
-    } else {
-        checkbox.checked = false;
-        product.bought = false;
-    }
-}
