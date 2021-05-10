8f76a6e3bea8eaaeb5cb91835828943dabf787a6
koala819 <x.genolhac@gmail.com>
2021-05-10 22:54:55 +0200

    Element title with react

---
+++ client/App/title.js
@@ -0,0 +1,7 @@
+class Title extends React.Component {
+    render() {
+      // Tells React what HTML code to render
+      return <h1>Ma liste de courses</h1>
+    }
+  }
+  ReactDOM.render(<Title />, document.getElementById("myTitle"))
--- client/src/like_button.js
+++ client/src/like_button.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const e = React.createElement;
+/*const e = React.createElement;
 
 class LikeButton extends React.Component {
   constructor(props) {
@@ -23,6 +23,15 @@ return (
 }
 
 const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(LikeButton), domContainer);*/
+class HelloWorld extends React.Component {
+  render() {
+    // Tells React what HTML code to render
+    return <h1>Hello World</h1>
+  }
+}
+
+//  Dis à React d'attacher le composant <HelloWorld> à la balise HTML DIV nommée 'root'
+ReactDOM.render(<HelloWorld />, document.getElementById("like_button_container"))
 
 //https://fr.reactjs.org/docs/add-react-to-a-website.html
