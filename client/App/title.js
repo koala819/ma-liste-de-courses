3a660646a5e56fa062c7052eed041f2ce7bd2018
koala819 <x.genolhac@gmail.com>
2021-05-12 13:21:55 +0200

    add product component

--- client/App/title.js
+++
@@ -1,7 +0,0 @@
-class Title extends React.Component {
-    render() {
-      // Tells React what HTML code to render
-      return <h1>Ma liste de courses</h1>
-    }
-  }
-  ReactDOM.render(<Title />, document.getElementById("myTitle"))
